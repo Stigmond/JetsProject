64948e9c320e25ece4c6e894b2297d9c858c4d7b
Stigmond <stigmond@gmail.com>
2020-09-12 21:53:46 -0600

    fill jet array and created menu

--- src/com/skilldistillery/jet/AirField.java
+++ src/com/skilldistillery/jet/AirField.java
@@ -1,16 +1,71 @@
 package com.skilldistillery.jet;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AirField {
 
-		ArrayList<Jet> jetArray;
-		
-		AirField(){
-			jetArray = new ArrayList<>();
-		}
-		
-		
-		
+	private List<Jet> jetsInAirfield;
+
+	AirField() {
+		jetsInAirfield = new ArrayList<>();
+		jetsInAirfield = fillJetArray();
 	}
 
+	protected List<Jet> fillJetArray() {
+
+		List<Jet> tempArray = new ArrayList<>();
+		try (BufferedReader bufIn = new BufferedReader(new FileReader("jets.txt"))) {
+			String line;
+			while ((line = bufIn.readLine()) != null) {
+				String[] fields = line.split(",");
+				if (fields[0].equals("Jet")) {
+					Jet tempJet = new JetImpl(fields[1], Double.parseDouble(fields[2]), Integer.parseInt(fields[3]),
+							Integer.parseInt(fields[4]));
+					tempArray.add(tempJet);
+				}
+				if (fields[0].equals("BomberJet")) {
+					Jet tempJet = new BomberJet(fields[1], Double.parseDouble(fields[2]), Integer.parseInt(fields[3]),
+							Integer.parseInt(fields[4]));
+					tempArray.add(tempJet);
+				}
+				if (fields[0].equals("CargoJet")) {
+					Jet tempJet = new CargoJet(fields[1], Double.parseDouble(fields[2]), Integer.parseInt(fields[3]),
+							Integer.parseInt(fields[4]));
+					tempArray.add(tempJet);
+				}
+				if (fields[0].equals("FighterJet")) {
+					Jet tempJet = new FighterJet(fields[1], Double.parseDouble(fields[2]), Integer.parseInt(fields[3]),
+							Integer.parseInt(fields[4]));
+					tempArray.add(tempJet);
+				}
+				if (fields[0].equals("AerialFireFighter")) {
+					Jet tempJet = new AerialFireFighter(fields[1], Double.parseDouble(fields[2]),
+							Integer.parseInt(fields[3]), Integer.parseInt(fields[4]));
+					tempArray.add(tempJet);
+				}
+			}
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+
+		return tempArray;
+	}
+
+	@Override
+	public String toString() {
+		return "AirField [jetsInAirfield=" + jetsInAirfield + "]";
+	}
+
+	public List<Jet> getJetsInAirfield() {
+		return jetsInAirfield;
+	}
+
+	public void setJetsInAirfield(List<Jet> jetsInAirfield) {
+		this.jetsInAirfield = jetsInAirfield;
+	}
+
+}
--- src/com/skilldistillery/jet/JetsApplication.java
+++ src/com/skilldistillery/jet/JetsApplication.java
@@ -1,33 +1,52 @@
 package com.skilldistillery.jet;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class JetsApplication {
 
 	private AirField airfield;
 	private Scanner input;
-	
-	JetsApplication(){
+
+	JetsApplication() {
 		airfield = new AirField();
 		input = new Scanner(System.in);
 	}
-	
+
 	public static void main(String[] args) {
-	
+
 		JetsApplication jetApp1 = new JetsApplication();
-		jetApp1.launch();	
-		
+		jetApp1.launch();
 	}
-	
+
 	public void launch() {
-		displayUserMenu();
+
+		List<Jet> jetArray = this.airfield.getJetsInAirfield();
+		
+		Menu();
 		input.close();
 	}
-	
-	public void displayUserMenu() {
-		Jet jet1 = new CargoJet("Spruce Goose", 350.00, 1500, 150000);
-		airfield.jetArray.add(jet1);
+
+	public void Menu() {
 		
-		airfield.jetArray.get(0).fly();
+		boolean keepGoing = true;
+		while (keepGoing) {
+		printMenu();
+		keepGoing = false;
+		}
+	}
+
+	public void printMenu() {
+		System.out.println("1. List Fleet");
+		System.out.println("2. Fly All Jets");
+		System.out.println("3. View Fastest Jet");
+		System.out.println("4. View Jet with Longest Range");
+		System.out.println("5. Load All Cargo Jets");
+		System.out.println("6. Dogfight!");
+		System.out.println("7. Add a Jet to Fleet");
+		System.out.println("8. Remove a Jet from Fleet");
+		System.out.println("9. Quit");
 	}
 }
+
